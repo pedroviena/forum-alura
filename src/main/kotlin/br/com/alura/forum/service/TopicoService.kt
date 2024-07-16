1e0642f98a22e5d331295079f486077979365135
Pedro Arian Viena <110788791+pedroviena@users.noreply.github.com>
2024-07-16 16:00:45 -0300

    Add files via upload

---
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -0,0 +1,50 @@
+package br.com.alura.forum.service
+
+import br.com.alura.forum.dto.AtualizacaoTopicoForm
+import br.com.alura.forum.dto.NovoTopicoForm
+import br.com.alura.forum.dto.TopicoView
+import br.com.alura.forum.exception.NotFoundException
+import br.com.alura.forum.mapper.TopicoFormMapper
+import br.com.alura.forum.mapper.TopicoViewMapper
+import br.com.alura.forum.model.Topico
+import br.com.alura.forum.repository.TopicoRepository
+import org.springframework.stereotype.Service
+import java.util.stream.Collectors
+
+@Service
+class TopicoService(
+    private val repository: TopicoRepository,
+    private val topicoViewMapper: TopicoViewMapper,
+    private val topicoFormMapper: TopicoFormMapper,
+    private val notFoundException: String = "Topico não encontrado!"
+) {
+
+    fun listar(): List<TopicoView> {
+        return repository.findAll().stream().map { t ->
+            topicoViewMapper.map(t)
+        }.collect(Collectors.toList())
+    }
+
+    fun buscarPorId(id: Long): TopicoView {
+        val topico = repository.findById(id)
+            .orElseThrow { NotFoundException(notFoundException) }
+        return topicoViewMapper.map(topico)
+    }
+
+    fun cadastrar(form: NovoTopicoForm): TopicoView {
+        val topico = topicoFormMapper.map(form)
+        repository.save(topico)
+        return topicoViewMapper.map(topico)
+    }
+
+    fun atualizar(form: AtualizacaoTopicoForm): TopicoView {
+        val topico = repository.findById(form.id).orElseThrow { NotFoundException(notFoundException) }
+        topico.titulo = form.titulo
+        topico.mensagem = form.mensagem
+        return topicoViewMapper.map(topico)
+    }
+
+    fun deletar(id: Long) {
+        repository.deleteById(id)
+    }
+}
